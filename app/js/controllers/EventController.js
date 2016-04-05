fc10de7fadec4c3ed8dccca18d8950d2e55a3941
Kenneth Mandawe <kdmandawe06@yahoo.com.ph>
2016-04-05 23:40:33 +0800

    Initial commit

---
+++ app/js/controllers/EventController.js
@@ -0,0 +1,21 @@
+'use strict';
+
+eventsApp.controller( 'EventController',
+    function EventController($scope, eventData, $routeParams, $route ){
+
+        $scope.sortorder = 'name';
+        $scope.event = $route.current.locals.event;
+        $scope.reload = function(){
+            $route.reload();
+        }
+
+        $scope.upVoteSession = function (session) {
+            session.upVoteCount++;
+        };
+
+        $scope.downVoteSession = function (session) {
+            session.upVoteCount--;
+        };
+
+    }
+)
---
+++ app/js/controllers/TimeoutSampleController.js
@@ -0,0 +1,13 @@
+'use strict'
+
+eventsApp.controller('TimeoutSampleController',
+    function TimoutSampleController($scope, $timeout){
+        var promise = $timeout(function() {
+            $scope.name = "John Doe";
+        }, 3000);
+
+        $scope.cancel = function(){
+            $timeout.cancel(promise);
+        }
+    }
+);
